1f07f1352aab780528e892e09072bb5c0d004b15
pigeonmal <eliastafnakji1@gmail.com>
2026-07-26 00:06:07 +0200

    feat: better icon export

--- packages/react-native-nano-icons/src/index.ts
+++ packages/react-native-nano-icons/src/index.ts
@@ -1,6 +1,7 @@
+export type { ColorValue } from 'react-native';
+export { createJSIIcon } from './createJSIIcon';
 export {
   createIconSet as createNanoIconSet,
   type IconComponent,
   type IconProps,
 } from './createNanoIconsSet';
-export type { ColorValue } from 'react-native';
