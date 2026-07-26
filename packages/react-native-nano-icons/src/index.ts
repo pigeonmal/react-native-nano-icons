b6af8ce12d8453921a0f60a750f2650f7536f6e4
pigeonmal <eliastafnakji1@gmail.com>
2026-07-26 13:47:38 +0200

    fix: export

--- packages/react-native-nano-icons/src/index.ts
+++ packages/react-native-nano-icons/src/index.ts
@@ -1,7 +1,2 @@
-export type { ColorValue } from 'react-native';
-export { createJSIIcon } from './createJSIIcon';
-export {
-  createIconSet as createNanoIconSet,
-  type IconComponent,
-  type IconProps,
-} from './createNanoIconsSet';
+export type { NanoGlyphMapInput } from './core/types';
+export { loadDynamicFont, useDynamicFontPending } from './loadDynamicFont';
