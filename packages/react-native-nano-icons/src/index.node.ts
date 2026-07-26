d6c9d307d69a1f4e1a135433d221460cada35744
pigeonmal <eliastafnakji1@gmail.com>
2026-07-26 15:17:19 +0200

    fix: node

--- packages/react-native-nano-icons/src/index.node.ts
+++ packages/react-native-nano-icons/src/index.node.ts
@@ -1,14 +1,6 @@
-type CreateIconSetFn = (...args: unknown[]) => unknown;
 type ConfigPluginFn = (...args: unknown[]) => unknown;
-
-const createNanoIconSet = (...args: unknown[]) => {
-  const { createIconSet } = require('./createNanoIconsSet') as {
-    createIconSet: CreateIconSetFn;
-  };
-  return createIconSet(...args);
-};
 
 const plugin = (require('./plugin/src/index') as { default: ConfigPluginFn })
   .default;
 
-module.exports = Object.assign(plugin, { createNanoIconSet });
+module.exports = Object.assign(plugin, {});
